132875f8e68b475da7fc88a59d89136623c567fa
Vineeta Hirani <vineetahirani@yahoo.com>
2024-03-12 23:29:57 +0530

    DashBoard.js

---
+++ DashBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = ({ stepCount }) => {
+  return (
+    <div>
+      <h2>Step Count: {stepCount}</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
