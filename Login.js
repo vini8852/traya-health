c783762212680ca6a4be1330d33dcbdc8d6809dd
Vineeta Hirani <vineetahirani@yahoo.com>
2024-03-12 23:27:45 +0530

    Login.js

---
+++ Login.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const LoginForm = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    onLogin(email, password);
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginForm;
